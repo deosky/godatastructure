bd3b607bc7c4eb91fedf1b90db31a44e132ae6a6
dev <npc00521@hotmail.com>
2016-08-21 16:57:41 +0800

    insert

--- list/list.go
+++ list/list.go
@@ -1,8 +1,11 @@
 package list
 
+import "errors"
+
 const (
-	LIST_INT_SIZE int = 100
+	LIST_INT_SIZE int = 10
 	LISTINCREMENT int = 10
+	ERROR         int = -1
 )
 
 type ElemType int
@@ -11,14 +14,33 @@ type Status int
 type SqList struct {
 	Elem     []ElemType
 	Length   int
-	listsize int
+	Listsize int
 }
 
-func InitList(l *SqList) Status {
+func InitSqList(l *SqList) Status {
 	array := [LIST_INT_SIZE]ElemType{}
 	l.Elem = array[:]
 	l.Length = 0
-	l.listsize = LIST_INT_SIZE
+	l.Listsize = LIST_INT_SIZE
 
 	return 1
+}
+
+//InsertSqList 在pos(从1开始)之前插入一个元素
+func InsertSqList(l *SqList, pos int, elem ElemType) (Status, error) {
+	if pos < 1 || pos > l.Length+1 {
+		return 0, errors.New("无效的插入位置")
+	}
+	if l.Length >= l.Listsize {
+		incrementArray := [LISTINCREMENT]ElemType{}
+		l.Elem = append(l.Elem, incrementArray[:]...)
+		l.Listsize = cap(l.Elem)
+	}
+	for i := l.Length - 1; i >= pos-1; i-- {
+		l.Elem[i+1] = l.Elem[i]
+	}
+	l.Elem[pos-1] = elem
+	l.Length++
+
+	return 1, nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import "godatastructure/list"
 import "fmt"
+import "strconv"
 
 func main() {
 	listNode := list.SqList{}
-	list.InitList(&listNode)
-	for i := 0; i < 100; i++ {
-		listNode.Elem[i] = list.ElemType(i)
-		listNode.Length++
+	list.InitSqList(&listNode)
+	for i := 0; i < 15; i++ {
+		list.InsertSqList(&listNode, i+1, list.ElemType(i))
+		fmt.Println("len = " + strconv.Itoa(listNode.Length) + " cap = " + strconv.Itoa(listNode.Listsize))
 	}
-	fmt.Println("abc")
-	fmt.Println(listNode)
+	//fmt.Println("abc")
+	fmt.Println(listNode.Length)
 }
